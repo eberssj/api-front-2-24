3c349a34739488e6d8862c91959fdabfbe9e9fdd
Erika Dias Ribeiro <diasribeiroerica4@gmail.com>
2024-09-12 08:03:31 -0300

    style: adicionei a estilização da sidebar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Routes } from "react-router-dom"
 import './App.css'
+import { Sidebar } from './components/Sidebar';
 
-export const App = () => {
+export default function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+      <Sidebar />
+        <Routes>
+          {/*<Route path="/" element={<Projetos />} />*/}
+        </Routes>
+    </Router>
   );
 }
