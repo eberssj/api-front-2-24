5848ebfb1f73b79d5cc7ea3b082b3525b3ccb7f6
Filipe Colla <filipe10colla@gmail.com>
2024-09-14 18:01:30 -0300

    Feat: Interface do Adm criada

---
+++ src/Type/Adm.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Telefone } from "./Telefone";
+
+export interface AdmProps {
+    id?: number,
+    nome?: string,
+    email?: string,
+    cpf?: string,
+    telefones?: Telefone[],
+    senha?: string,
+    tipo?: any,
+    dataCadastro?: Date,
+    sub?: string,
+    ativo?: boolean,
+    token?: string,
+}
