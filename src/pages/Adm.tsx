73bd371fe0f424665d259dce7ed46a9ae1eef6fa
Filipe Colla <filipe10colla@gmail.com>
2024-09-14 17:58:47 -0300

    Feat: Pagina do adm criada

---
+++ src/pages/Adm.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Adm() {
+
+    return (
+        <div>
+            <h1>Adm</h1>
+            <Link to="/adm">Go to Home</Link>
+        </div>
+    )
+}
