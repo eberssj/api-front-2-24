1a6e9f999ac9e1c2397d3f77f9ad28a765acfe15
Filipe Colla <filipe10colla@gmail.com>
2024-09-14 17:59:56 -0300

    Feat: Pagina de rota n achada feita

---
+++ src/pages/PageNotFounded.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function PageNotFounded() {
+    return (
+        <div>
+            <h1>Page Not Founded</h1>
+            <Link to="/">Go to Home</Link>
+        </div>
+    )
+}
