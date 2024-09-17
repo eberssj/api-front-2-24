280a99c4a26a9b236f67724389c7f97efe56fd23
Filipe Colla <filipe10colla@gmail.com>
2024-09-17 16:51:48 -0300

    Fix: Ajuste nos importes

--- src/pages/Adm.tsx
+++ src/pages/Adm.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { Sidebar } from "../components/Sidebar"
+import { Sidebar } from "../components/Sidebar/Sidebar"
 
 export default function Adm() {
 
