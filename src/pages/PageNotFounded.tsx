60428dd3e5fd06eeb4c060b631fa57f4dd0733ca
Filipe Colla <filipe10colla@gmail.com>
2024-09-22 16:06:29 -0300

    Refactor: Pagina de não encontrado com codigo aprimorado

--- src/pages/PageNotFounded.tsx
+++ src/pages/PageNotFounded.tsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
-
 export default function PageNotFounded() {
+    
     return (
         <div>
-            <h1>Page Not Founded</h1>
-            <Link to="/">Go to Home</Link>
+            <h1>Error! 404</h1>
+            <h1>Page Not Founded!</h1>
+
+            <a href="/"><br/>Go to Home</a>
         </div>
     )
 }
