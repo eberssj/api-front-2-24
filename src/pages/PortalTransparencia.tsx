199d2e5d6f482871311a0eefb429e62b23f3dddc
Filipe Colla <filipe10colla@gmail.com>
2024-10-02 15:54:35 -0300

    Feat: Adicionada a funcionalidade de filtrar

--- src/pages/PortalTransparencia.tsx
+++ src/pages/PortalTransparencia.tsx
@@ -8,12 +8,19 @@ import { useNavigate } from 'react-router-dom';
 const PortalTransparencia = () => {
     const navigate = useNavigate();
     const [projetos, setProjetos] = useState<Projeto[]>([]);
+    const [projetosFiltrados, setProjetosFiltrados] = useState<Projeto[]>([]);
+    const [referenciaProjeto, setReferenciaProjeto] = useState('');
+    const [coordenador, setCoordenador] = useState('');
+    const [valorProjeto, setValorProjeto] = useState('');
+    const [dataInicio, setDataInicio] = useState('');
+    const [dataTermino, setDataTermino] = useState('');
 
     useEffect(() => {
         const fetchProjetos = async () => {
             try {
                 const response = await axios.get('http://localhost:8080/projeto/listar');
                 setProjetos(response.data);
+                setProjetosFiltrados(response.data);
             } catch (error) {
                 console.error(error);
             }
@@ -22,15 +29,41 @@ const PortalTransparencia = () => {
         fetchProjetos();
     }, []);
 
-    const formatarData = (dataArray: number[]) => {
+    const formatarDataParaComparacao = (dataArray: number[]) => {
         if (Array.isArray(dataArray) && dataArray.length === 3) {
-            return new Date(dataArray[0], dataArray[1] - 1, dataArray[2]).toLocaleDateString('pt-BR', {
-                day: 'numeric',
-                month: 'numeric',
-                year: 'numeric'
-            });
+            const [ano, mes, dia] = dataArray.map(num => String(num).padStart(2, '0'));
+            return `${ano}-${mes}-${dia}`;
         }
-        return 'Data inválida';
+        return '';
+    };
+
+    const formatarDataParaVisualizacao = (dataArray: number[]) => {
+        if (Array.isArray(dataArray) && dataArray.length === 3) {
+            const [ano, mes, dia] = dataArray.map(num => String(num).padStart(2, '0'));
+            return `${dia}/${mes}/${ano}`;
+        }
+        return '';
+    };
+
+    const aplicarFiltro = () => {
+        const projetosFiltrados = projetos.filter((projeto) => {
+            const referenciaProjetoMatch = referenciaProjeto ? projeto.referenciaProjeto.toLowerCase().includes(referenciaProjeto.toLowerCase()) : true;
+            const coordenadorMatch = coordenador ? projeto.coordenador.toLowerCase().includes(coordenador.toLowerCase()) : true;
+            const valorProjetoMatch = valorProjeto ? projeto.valor.toString().includes(valorProjeto) : true;
+
+            const dataInicioFormatada = formatarDataParaComparacao(projeto.dataInicio);
+            const dataTerminoFormatada = formatarDataParaComparacao(projeto.dataTermino);
+
+            const dataInicioFiltro = dataInicio.split('/').reverse().join('-');
+            const dataTerminoFiltro = dataTermino.split('/').reverse().join('-');
+
+            const dataInicioMatch = dataInicio ? dataInicioFormatada >= dataInicioFiltro : true;
+            const dataTerminoMatch = dataTermino ? dataTerminoFormatada <= dataTerminoFiltro : true;
+
+            return referenciaProjetoMatch && coordenadorMatch && valorProjetoMatch && dataInicioMatch && dataTerminoMatch;
+        });
+
+        setProjetosFiltrados(projetosFiltrados);
     };
 
     return (
@@ -40,13 +73,54 @@ const PortalTransparencia = () => {
                 <h1>Fundação de Apoio à Pesquisa de Pós-Graduandos (FAPG)</h1>
                 <p>A Fundação vem com o objetivo de trazer transformações importantes para o meio acadêmico, e busca conseguir isso com a obstinação dos seus dirigentes <br /> e funcionários no sentido de torná-la sempre mais ágil e precisa.</p>
                 <br />
-                {projetos.length > 0 ? (
+                <div className="filtros">
+                    <form onSubmit={(e) => { e.preventDefault(); aplicarFiltro(); }}>
+                        <input
+                            type="text"
+                            placeholder="Referência do projeto"
+                            value={referenciaProjeto}
+                            onChange={(e) => setReferenciaProjeto(e.target.value)}
+                            className="input-filtrar-projetos"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Coordenador"
+                            value={coordenador}
+                            onChange={(e) => setCoordenador(e.target.value)}
+                            className="input-filtrar-projetos"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Valor do projeto"
+                            value={valorProjeto}
+                            onChange={(e) => setValorProjeto(e.target.value)}
+                            className="input-filtrar-projetos"
+                        />
+                        <input
+                            type="date"
+                            placeholder="Data de início"
+                            value={dataInicio}
+                            onChange={(e) => setDataInicio(e.target.value)}
+                            className="input-filtrar-projetos"
+                        />
+                        <input
+                            type="date"
+                            placeholder="Data de término"
+                            value={dataTermino}
+                            onChange={(e) => setDataTermino(e.target.value)}
+                            className="input-filtrar-projetos"
+                        />
+                        <button type="submit" className="botao-filtrar-projetos">Aplicar Filtros</button>
+                    </form>
+                </div>
+                <br />
+                {projetosFiltrados.length > 0 ? (
                     <>
                         <p>Abaixo estão todos os projetos desenvolvidos.</p>
                         <br />
                         <div className="container-projetos-externo">
                             <div>
-                                {projetos.map((projeto) => (
+                                {projetosFiltrados.map((projeto) => (
                                     <div className="container-projeto" key={projeto.id}>
                                         <div className="itens-esquerda">
                                             <p><strong>Referência do projeto:</strong> {projeto.referenciaProjeto}</p>
@@ -54,8 +128,8 @@ const PortalTransparencia = () => {
                                             <p><strong>Valor:</strong> R$:{projeto.valor}</p>
                                         </div>
                                         <div className="itens-meio">
-                                            <p><strong>Início:</strong> {formatarData(projeto.dataInicio)}</p>
-                                            <p><strong>Término:</strong> {formatarData(projeto.dataTermino)}</p>
+                                            <p><strong>Início:</strong> {formatarDataParaVisualizacao(projeto.dataInicio)}</p>
+                                            <p><strong>Término:</strong> {formatarDataParaVisualizacao(projeto.dataTermino)}</p>
                                         </div>
                                         <div className="itens-direita cursor-pointer" onClick={() => navigate(`/projeto/${projeto.id}`, { state: projeto })}>
                                             <i className="bi bi-file-earmark-text"></i>
