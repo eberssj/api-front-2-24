46a87e60c2148f0d40f8b4cf10adfc6bca644500
Juan Garcia Soares <juan.garcia.soares@gmail.com>
2024-10-08 22:15:37 -0300

    feat: implementação do componente AutoComplete genérico para projetos

---
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Projeto } from '../../Type/Projeto';
+import '../../styles/PortalTransparencia.css'
+
+interface AutoCompleteProps {
+    prop: keyof Projeto;
+    onValueChange: (value: string) => void;
+}
+
+const AutoComplete: React.FC<AutoCompleteProps> = ({ prop, onValueChange }) => {
+    const [opcoes, setOpcoes] = useState<string[]>([]);
+    const [valorSelecionado, setValorSelecionado] = useState('');
+
+    useEffect(() => {
+        const fetchProjetos = async () => {
+            try {
+                const response = await axios.get<Projeto[]>('http://localhost:8080/projeto/listar');
+                const projetos = response.data;
+                const opcoesEspecificas = Array.from(new Set(projetos.map((projeto) => projeto[prop] as string)));
+                setOpcoes(opcoesEspecificas);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchProjetos();
+    }, [prop]);
+
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+        setValorSelecionado(value);
+        onValueChange(value);
+    };
+
+    return (
+        <div>
+            <select
+                value={valorSelecionado}
+                onChange={handleSelectChange}
+                className="input-padrao"
+            >
+                <option value="">Escolha uma opção</option>
+                {opcoes.map((opcao, index) => (
+                    <option key={index} value={opcao}>
+                        {opcao}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default AutoComplete;
