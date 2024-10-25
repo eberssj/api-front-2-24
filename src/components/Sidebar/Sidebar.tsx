12da7d987f925efd339da68d4b37a25bd851a585
Marco Antonio Arantes <marcoantoniopou@gmail.com>
2024-10-25 20:47:27 -0300

    refactor: Refazendo todo o estilo do componente Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,161 +1,110 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useAuth } from '../../hook/Auth';
 import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from '../../hook/ContextAuth';
 import { Toast } from '../Swal/Swal';
-import "../../styles/Sidebar.css"
-import iconeNotificacao from "./notificacao.svg"
+import "../../styles/Sidebar.css";
 
 export const Sidebar: React.FC = () => {
-  const [selected, setSelected] = useState('Projetos');
   const { logout } = useAuth();
+  const location = useLocation();
   const navigate = useNavigate();
   const { adm } = useContext(AuthContext);
 
+  const isAtivoProjetos = location.pathname === "/adm/projetos";
+  const isAtivoDashboard = location.pathname === "/adm/dashboard";
+  const isAtivoAdministradores = location.pathname === "/adm/administradores";
+  const isAtivoNotificacoes = location.pathname === "/adm/notificacoes";
+
+  useEffect(() => {
+    document.body.classList.toggle('no-margin', !adm);
+    return () => {
+      document.body.classList.remove('no-margin');
+    };
+  }, [adm]);
+
   const handleSignout = () => {
     Swal.fire({
-        title: "Deseja realmente sair?",
-        showDenyButton: true,
-        confirmButtonText: "Sim",
-        denyButtonText: "Não",
-        width: 410,
-        confirmButtonColor: 'rgb(255, 0, 53)',
-        denyButtonColor: 'rgb(0,114,187)',
-        heightAuto: false,
-        backdrop: false, 
-        customClass: { 
-            confirmButton: 'cButton',
-            denyButton: 'dButton',
-
-        }
-
+      title: "Deseja realmente sair?",
+      showDenyButton: true,
+      confirmButtonText: "Sim",
+      denyButtonText: "Não",
+      width: 410,
+      confirmButtonColor: 'rgb(255, 0, 53)',
+      denyButtonColor: 'rgb(0,114,187)',
+      heightAuto: false,
+      backdrop: false, 
+      customClass: { 
+        confirmButton: 'cButton',
+        denyButton: 'dButton',
+      }
     }).then((result) => {
-        if (result.isConfirmed) {
-            Toast.fire({
-                icon: 'success',
-                title: 'Logout realizado com sucesso!'
-            });
-            logout();
-            navigate("/login");
-        }
+      if (result.isConfirmed) {
+        Toast.fire({
+          icon: 'success',
+          title: 'Logout realizado com sucesso!'
+        });
+        logout();
+        navigate("/login");
+      }
     });
-  }
+  };
 
-  const renderMenuAdm   = () => {
+  const renderMenuAdm = () => {
     return (
       <div className="sidebar">
-        <div className='logo-items-container'>
-          <div className="logo-container">
-                <img 
-                  src="../src/img/logo.jpeg"
-                  alt="Logo"
-                  className='logo-img'
-                />
+        <div className="side_cima">
+          <div className="side_logotipo">
+            <img src="../src/img/logotipo_fapg.svg" alt="Logo" className="logo-img" />
           </div>
 
-          <div className='items'>
-          <li>
-              <a
-                href="/adm/projetos"
-                onClick={() => setSelected('Projetos')}
-                className={`flex items-center px-4 py-2 ${selected === 'Projetos' ? 'bg-blue-800 text-white rounded-md' : 'rounded-md text-gray-600 hover:bg-gray-200 hover:text-gray-900'} transition-all duration-300`}
-              >
-                <span className="mr-4">
-                  <i className="bi bi-calendar mr-2"></i>
-                  <p>Projetos</p>
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="/adm/dashboard"
-                onClick={() => setSelected('Dashboard')}
-                className={`flex items-center px-4 py-2 ${selected === 'Projetos' ? 'bg-blue-800 text-white rounded-md' : 'rounded-md text-gray-600 hover:bg-gray-200 hover:text-gray-900'} transition-all duration-300`}
-              >
-                <span className="mr-4">
-                  <i className="bi bi-graph-up mr-2"></i>
-                  <p>Dashboard</p>
-                </span>
-              </a>
-            </li>
-            <li>
-            <a
-                href="/adm/administradores"
-                onClick={() => setSelected('Administradores')}
-                className={`flex items-center px-4 py-2 ${selected === 'Projetos' ? 'bg-blue-800 text-white rounded-md' : 'rounded-md text-gray-600 hover:bg-gray-200 hover:text-gray-900'} transition-all duration-300`}
-              >
-                <span className="mr-4">
-                  <i className="bi bi-graph-up mr-2"></i>
-                  <p>Administradores</p>
-                </span>
-              </a>
-            </li>
-            <li>
-            <a
-                href="/adm/notificacoes"
-                onClick={() => setSelected('Notificacoes')}
-                className={`flex items-center px-4 py-2 ${selected === 'Projetos' ? 'bg-blue-800 text-white rounded-md' : 'rounded-md text-gray-600 hover:bg-gray-200 hover:text-gray-900'} transition-all duration-300`}
-              >
-                <span className="mr-4">
-                  <img className="icon_notificacao" src={iconeNotificacao} />
-                  <p>Notificações</p>
-                </span>
-              </a>
-            </li>
-          </div>        
-        </div>
-  
-        {/* Logout */}
-        <div>
-          <p className='logout' onClick={handleSignout}>
-            <i className="bi bi-box-arrow-right logout-icon"></i>
-            <p>Logout</p> 
-          </p>
-
-        {/* Admin Info */}
-        <div className="adm-container">
-          <i className="bi bi-person-circle perfil-icon"></i>
-            <div className='adm-info'>
-              <p className='adm-nome'>Administrador: {adm?.nome}</p>
-              <p className="adm-email">{adm?.email}</p>
-            </div>
-        </div>
-      </div>
-    </div>
-    );
-  }
-
-  const renderMenuUser = () => {
-    return (
-      <div className="sidebar">
-          <div className="logo-items-container">
-              <div className="logo-container">
-                <img 
-                  src="../src/img/logo.jpeg"
-                  alt="Logo"
-                  className="logo-img" 
-                />
-              </div>
-  
           <div className="items">
-            <li>
-              <a
-                href="/"
-                onClick={() => setSelected('Projetos')}
-                className={`flex items-center px-4 py-2 ${selected === 'Projetos' ? 'bg-blue-800 text-white rounded-md' : 'rounded-md text-gray-600 hover:bg-gray-200 hover:text-gray-900'} transition-all duration-300`}
-              >
-                <span className="mr-4">
-                  <i className="bi bi-calendar mr-2"></i>
-                  <p>Projetos</p>
-                </span>
-              </a>
-            </li>
+
+            <div className={`side_botao ${isAtivoProjetos ? "ativo" : ""}`} onClick={() => navigate('/adm/projetos')}>
+              <img src={isAtivoProjetos ? "../src/img/pasta_active.svg" : "../src/img/pasta.svg"} alt="Projetos" />
+              <p>Projetos</p>
+            </div>
+
+            <div className={`side_botao ${isAtivoDashboard ? "ativo" : ""}`} onClick={() => navigate('/adm/dashboard')}>
+              <img src={isAtivoDashboard ? "../src/img/dashboard_active.svg" : "../src/img/dashboard.svg"} alt="Dashboard" />
+              <p>Dashboard</p>
+            </div>
+
+            <div className={`side_botao ${isAtivoAdministradores ? "ativo" : ""}`} onClick={() => navigate('/adm/administradores')}>
+              <img src={isAtivoAdministradores ? "../src/img/user_active.svg" : "../src/img/user.svg"} alt="Administradores" />
+              <p>Administradores</p>
+            </div>
+
+            <div className={`side_botao ${isAtivoNotificacoes ? "ativo" : ""}`} onClick={() => navigate('/adm/notificacoes')}>
+              <img src={isAtivoNotificacoes ? "../src/img/notificacao_active.svg" : "../src/img/notificacao.svg"} />
+              <p>Notificações</p>
+            </div>
+
           </div>
-        </div>
+          </div>
+
+          {/* Admin Info */}
+
+          <div className="side_baixo">
+            <div className="side_baixo_logout">
+              <img src="../src/img/logado_user.svg" />
+              <div className="side_info">
+                <p className="side_nome">{adm?.nome}</p>
+                <p className="side_email">{adm?.email}</p>
+              </div>
+            </div>
+
+          {/* Logout */}
+
+            <div className="side_logout" onClick={handleSignout}>
+              <img src="../src/img/logout.svg" />
+              <p>Logout</p> 
+            </div>
+          </div>
       </div>
     );
-  }
+  };
 
   if (adm) {
     return (
@@ -163,11 +112,8 @@ export const Sidebar: React.FC = () => {
         {renderMenuAdm()}
       </div>
     );
-  } else {
-    return (
-      <div>
-        {renderMenuUser()}
-      </div>
-    );
   }
+
+  return null;
+  
 };
