780c3ebd93aec25b90f895ebfe834f2b74737676
Juan Garcia Soares <juan.garcia.soares@gmail.com>
2024-11-05 11:34:38 -0300

    feat: implementando o componente NotificacaoAlerta para receber os dados dos projetos a vencer

--- src/components/NotificacaoAlerta/NotificacaoAlerta.tsx
+++ src/components/NotificacaoAlerta/NotificacaoAlerta.tsx
@@ -1,28 +1,36 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./NotificacaoAlerta.css";
-import Fechar from "./fechar.svg"
 
-function NotificacaoAlerta() {
-  
-  return (
-    
-   <div className="noale_container">
-
-        <div className="noale_cima">
-          <h1>PROJETO ID 42</h1>
-          <img src={Fechar} />
-        </div>
-        
-        <div className="noale_hr">
-        </div>
-
-        <p className="noale_aviso">Este projeto vencerá em um dia.</p>
-
-        <p>Data de Início: 20/10/2022</p>
-        <p>Data de Término: 25/4/2024</p>
-
-    </div>
-
-  );
+interface NotificacaoAlertaProps {
+  id: number;
+  dataInicio: number[];
+  dataTermino: number[];
+  diasParaVencer: number;
 }
+
+const formatarDataParaVisualizacao = (dataArray: number[]) => {
+  if (Array.isArray(dataArray) && dataArray.length === 3) {
+    const [ano, mes, dia] = dataArray.map(num => String(num).padStart(2, '0'));
+    return `${dia}/${mes}/${ano}`;
+  }
+  return '';
+};
+
+const NotificacaoAlerta: React.FC<NotificacaoAlertaProps> = ({ id, dataInicio, dataTermino, diasParaVencer }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="noale_container">
+      <div className="noale_cima">
+        <h1 onClick={() => navigate(`/projeto/editar/${id}`)}>PROJETO ID {id}</h1>
+      </div>
+      <div className="noale_hr"></div>
+      <p className="noale_aviso">{diasParaVencer === 0 ? "Este projeto vencerá HOJE." : `Este projeto vencerá em ${diasParaVencer} dia(s).`} </p>
+      <p>Data de Início: {formatarDataParaVisualizacao(dataInicio)}</p>
+      <p>Data de Término: {formatarDataParaVisualizacao(dataTermino)}</p>
+    </div>
+  );
+};
 
 export default NotificacaoAlerta;
